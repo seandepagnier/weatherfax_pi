90e7856fff71203a14392db2fb86ab2a0f9e768a
Sean D'Epagnier <sean@depagnier.com>
2014-07-25 03:57:32 +0800

    fix build for mingw, and disable visual studio warnings

--- src/libaudiofile/config.h
+++ src/libaudiofile/config.h
@@ -3,6 +3,8 @@
   typedef int ssize_t;
   #define snprintf  _snprintf
   #define HAVE_UNISTD_H 0
+
+  #pragma warning(disable: 4244)
 #else
   /* Define to 1 if you have the <unistd.h> header file. */
   #define HAVE_UNISTD_H 1
