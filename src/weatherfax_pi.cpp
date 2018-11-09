1166b060d5198c4a39e6fa5a7ced887fbe7aa58c
didier <didier@users.sourceforge.net>
2018-11-09 10:31:42 +0100

    OSX refactoring, missed a std_path stdPath rename

--- src/weatherfax_pi.cpp
+++ src/weatherfax_pi.cpp
@@ -244,10 +244,14 @@ wxString weatherfax_pi::StandardPath()
 #ifdef __WXOSX__
     // Compatibility with pre-OCPN-4.2; move config dir to
     // ~/Library/Preferences/opencpn if it exists
-    wxString oldPath = (std_path.GetUserConfigDir() + s + _T("plugins") + s + _T("weatherfax"));
-    if (wxDirExists(oldPath) && !wxDirExists(stdPath)) {
-	wxLogMessage("weatherfax_pi: moving config dir %s to %s", oldPath, stdPath);
-	wxRenameFile(oldPath, stdPath);
+    {
+        wxStandardPathsBase& std_path = wxStandardPathsBase::Get();
+        wxString s = wxFileName::GetPathSeparator();
+        wxString oldPath = (std_path.GetUserConfigDir() + s + _T("plugins") + s + _T("weatherfax"));
+        if (wxDirExists(oldPath) && !wxDirExists(stdPath)) {
+		    wxLogMessage("weatherfax_pi: moving config dir %s to %s", oldPath, stdPath);
+		    wxRenameFile(oldPath, stdPath);
+        }
     }
 #endif
 
