5723e69984967f89b2b6d450270bc1df2d84cbc5
Rick Gleason <fcgleason@tgp-architects.com>
2019-03-04 17:09:06 -0500

    remove define snprintf from libaudiofile config.h

--- src/libaudiofile/config.h
+++ src/libaudiofile/config.h
@@ -1,7 +1,7 @@
 #if defined(__MSVC__)
   #include <BaseTsd.h>
   typedef int ssize_t;
-  #define snprintf  _snprintf
+/*  #define snprintf  _snprintf
   #define HAVE_UNISTD_H 0
 
   #pragma warning(disable: 4244)
