ce43a56451505e92dcbb3587339f080bdf7a46e0
jongough <jon.gough@eclipsesystems.com.au>
2020-05-20 11:23:10 +1000

    Update cp to mv to remove file from build directory

--- ci/circleci-build-mingw.sh
+++ ci/circleci-build-mingw.sh
@@ -12,8 +12,8 @@ rm -rf build; mkdir build; cd build
 wget https://downloads.sourceforge.net/project/opencpnplugins/opencpn_packaging_data/PVW32Con.exe
 wget https://downloads.sourceforge.net/project/opencpnplugins/opencpn_packaging_data/portaudio-vc12.7z
 7za e portaudio-vc12.7z -o../buildwin -y
-cp PVW32Con.exe ../buildwin/.
+mv PVW32Con.exe ../buildwin/.
 cmake -DCMAKE_TOOLCHAIN_FILE=../mingw/fedora/toolchain.cmake ..
 make -j2
 make package
-rm ../build/PVW32Con.exe
+
