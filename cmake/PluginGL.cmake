9c2b38aed0f8e17bd9b16b2ae465396126253f39
Rick Gleason <fcgleason@tgp-architects.com>
2021-05-26 10:46:47 -0400

    v1.9.14.11 + clnup

--- cmake/PluginGL.cmake
+++ cmake/PluginGL.cmake
@@ -11,6 +11,20 @@ SET(SRC_PLUGINGL
           src/plugingl/qtstylesheet.cpp
           )
 
+#  from cmake  -not needed see cmake/PluginGL.cmake  Line 594
+#SET (SRC_PLUGINGL
+#			src/plugingl/linmath.h
+#			src/plugingl/pi_shaders.h
+#			src/plugingl/pidc.h
+#			src/plugingl/qtstylesheet.h
+#			src/plugingl/TexFont.h
+#           src/plugingl/pi_shaders.cpp
+#			src/plugingl/pidc.cpp
+#			src/plugingl/qtstylesheet.cpp
+#			src/plugingl/TexFont.cpp
+#)
+
+
 if(UNIX)
     add_definitions("-fpic")
 endif(UNIX)
--- cmake/PluginXML.cmake
+++ cmake/PluginXML.cmake
@@ -5,14 +5,23 @@
 ##---------------------------------------------------------------------------
 
 
-IF(WIN32)
+# IF(WIN32)   -changed
 SET(SRC_TINYXML
             src/tinyxml/tinyxml.cpp
             src/tinyxml/tinyxmlparser.cpp
             src/tinyxml/tinyxmlerror.cpp
 )
+
+# from cmake  not needed see cmake/PluginXML.cmake  Line 593
+#SET (SRC_TINYXML
+#            src/tinyxml/tinyxml.cpp
+#			src/tinyxml/tinyxmlerror.cpp
+#			src/tinyxml/tinyxmlparser.cpp
+#			src/tinyxml/tinyxml.h
+#)
+
 ADD_LIBRARY(${PACKAGE_NAME}_LIB_PLUGINXML STATIC ${SRC_TINYXML})
 TARGET_LINK_LIBRARIES( ${PACKAGE_NAME} ${PACKAGE_NAME}_LIB_PLUGINXML )
-ENDIF(WIN32)
+# ENDIF(WIN32)
 
 ADD_DEFINITIONS(-DTIXML_USE_STL)
