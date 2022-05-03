61c5011fc9507e3bfdf628e450384103c6587bf9
Rick Gleason <fcgleason@tgp-architects.com>
2022-05-03 09:07:38 -0400

    1.9.30.1 chng update url

--- src/weatherfax_pi.cpp
+++ src/weatherfax_pi.cpp
@@ -332,7 +332,8 @@ bool weatherfax_pi::LoadConfig(void)
     pConf->Read ( _T ( "SoundingDatum" ), &m_sExportSoundingDatum, _T("LOWEST LOW WATER"));
 
     pConf->SetPath ( _T ( "/Settings/WeatherFax/Updates" ) );
-    pConf->Read( _T("UpdateDataBaseUrl"), &m_UpdateDataBaseUrl, _T("https://raw.githubusercontent.com/rgleason/weatherfax_pi/master/data/") );
+    pConf->Read( _T("UpdateDataBaseUrl"), &m_UpdateDataBaseUrl, _T(
+	"https://github.com/rgleason/weatherfax_pi/blob/master/data/") );//"https://raw.githubusercontent.com/rgleason/weatherfax_pi/master/data/") );
 
     return true;
 }
