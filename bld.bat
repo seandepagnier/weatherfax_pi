1512f77f9776263ad601003e5b8a08119a79fac0
Rick Gleason <fcgleason@tgp-architects.com>
2023-01-10 19:07:20 -0500

    fix aviation wx water vapor

--- bld.bat
+++ bld.bat
@@ -21,6 +21,8 @@ REM      -DCMAKE_BUILD_TYPE=RelWithDebInfo  ..
 REM cmake --build . --target tarball --config RelWithDebInfo >output.txt
 
 REM  For Opencpn 5.7.1 and wxWidgets-3.2.1
+
+
 cd build
 cmake -T v143 -A Win32 ..
 cmake --build . --target package --config relwithdebinfo >output.txt
