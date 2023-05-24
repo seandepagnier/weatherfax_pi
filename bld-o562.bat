c66a42dc07c874aae973c6c0f8c2c242b3cfe8b6
Rick Gleason <fcgleason@tgp-architects.com>
2023-05-24 07:52:24 -0400

    1.9.57 + TP1.0.237

--- bld-o562.bat
+++
@@ -1,4 +0,0 @@
-cd build
-cmake -T v141_xp -DOCPN_TARGET=MSVC ..
-cmake --build . --target package --config release >output.txt
-bash ./cloudsmith-upload.sh
--- bld-o571.bat
+++
@@ -1,4 +0,0 @@
-cd build
-cmake -T v143 -A Win32 -DOCPN_TARGET=MSVC ..
-cmake --build . --target package --config relwithdebinfo >output.txt
-bash ./cloudsmith-upload.sh
