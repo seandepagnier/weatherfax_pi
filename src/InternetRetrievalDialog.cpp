0f8c717a8ab6487d70b8b3eb76cc1a4c7ed1f0df
Dave <bdbcat@yahoo.com>
2025-01-03 11:03:42 -0500

    Cleanup

--- src/InternetRetrievalDialog.cpp
+++ src/InternetRetrievalDialog.cpp
@@ -39,11 +39,6 @@
 #include "WeatherFaxImage.h"
 #include "WeatherFax.h"
 
-//#ifndef __OCPN__ANDROID__
-// #include "wx/curl/ftp.h"
-// #include "wx/curl/dialog.h"
-//#endif
-
 InternetRetrievalDialog::InternetRetrievalDialog( weatherfax_pi &_weatherfax_pi, wxWindow* parent)
 #ifndef __WXOSX__
     : InternetRetrievalDialogBase( parent ),
